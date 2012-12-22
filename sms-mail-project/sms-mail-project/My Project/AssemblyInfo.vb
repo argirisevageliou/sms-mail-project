d1ad8b037b6cf76ab7d16e6b4f366618c2c297d3
Greg <krohsgrhgorhs@gmail.com>
2012-12-23 01:29:07 +0200

    Last modifications to AboutBox form and Project's settings

--- sms-mail-project/sms-mail-project/My Project/AssemblyInfo.vb
+++ sms-mail-project/sms-mail-project/My Project/AssemblyInfo.vb
@@ -9,7 +9,7 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("Sms & Email Application Center -  Free Edition")> 
-<Assembly: AssemblyDescription("This Desktop Application helps you create your own emails or messages and send them to other receivers that may have mobile phones with the sms section or to other devices that can read emails. Also you can send messages(sms & email) with multi threading option that gives you the chance to send more than one message to the other receivers simultaneously.")> 
+<Assembly: AssemblyDescription("This Desktop Application helps you create your own emails or messages and send them to other receivers that may have mobile phones with the sms section or to other devices that can read emails. Also you can send messages(sms & email) with multi threading option that gives you the chance to send more than one message to the other receivers simultaneously.                                                     Developers : Anarguros Evageliou , Efthimios Ziogas , Grhgorios Krohs")> 
 <Assembly: AssemblyCompany("Semester Project")> 
 <Assembly: AssemblyProduct("Sms & Email Application Center -  Free Edition")> 
 <Assembly: AssemblyCopyright("Copyright 2012 - 2013 All Rights Reserved")> 
